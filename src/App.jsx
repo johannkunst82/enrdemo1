c446ee0b7ef23c250ee5bb8883958d5947f85490
Johann Kunst <johann.kunst82@gmail.com>
2025-03-18 21:13:27 +0100

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,6 @@ import Footer from './Footer.jsx';
 import HeaderComp from './HeaderComp.jsx'
 import reactLogo from './assets/react.svg'
 
-/**
- * This is the main App component.
- * It renders a header with links to the Vite and React documentation,
-
- * and a message that tells you to edit the file to test HMR.
- */
-
-
 function MainEntry() {
   return (
   <Router>
@@ -30,7 +22,8 @@ function MainEntry() {
 
 function FirstPage() {
 
-  const [input, setInput] = useState('Bitte aus meiner Daten beantworten, mit genaue Quellenangabe: unterliegen Energieerzeugnisse der Energiesteuer?');
+//  const [input, setInput] = useState('Bitte aus meiner Daten beantworten, mit genaue Quellenangabe: unterliegen Energieerzeugnisse der Energiesteuer?');
+  const [input, setInput] = useState('');
   const [botResponse, setBotResponse] = useState('');
   const [loading, setLoading] = useState(false);
 
@@ -46,7 +39,7 @@ function FirstPage() {
     const apiKey = "FX6ANBefu2qbluscjYo37YXcLOwRUpKXzruvNGWFlRquXznquOyIJQQJ99BCAC4f1cMXJ3w3AAAAACOG3rMm";
         
     const fetchAnswer = async () => {    
-    const dataField = '{ "messages": [  { "role": "user",  "content": "' + input + '" } ],   "max_tokens": 1000, "temperature": 0.7, "top_p": 1, "stop": []}'
+    const dataField = '{ "messages": [  { "role": "user",  "content": "' + input.trim() + '" } ],   "max_tokens": 1500, "temperature": 0.7, "top_p": 1, "stop": []}'
 //const dataField =  {"data_sources": [{"type": "azure_search","parameters": {"endpoint": url, "index_name": "v3index2","semantic_configuration": "default","query_type": "vector","fields_mapping": {},"in_scope": true,"role_information": "Sie sind KI-Assistent und helfen Personen, Informationen zu finden.", "filter": null,"strictness": 4, "top_n_documents": 20, "authentication": {"type": "api_key","key": apiKey},"embedding_dependency": {"type": "gpt-35-turbo","deployment_name": "text-embedding-ada-002"},"key": "'$search_key'","indexName": "'$search_index'"}}],  "messages": [{"role": "system","content": "Sie sind KI-Assistent und helfen Personen, Informationen zu finden." }],  "temperature": 0.7,"top_p": 0.95,"max_tokens": 800,"stop": null,"stream": true,"past_messages": 10,"frequency_penalty": 0,"presence_penalty": 0,"azureSearchEndpoint": "https://v3azurekisearchservice.search.windows.net",  "azureSearchKey": "***","azureSearchIndexName": "v3index2"};
       
       const response = await fetch(url, {
@@ -59,9 +52,6 @@ function FirstPage() {
       });
     
 
-
-      const message = "Entered: " + input;
-      console.log(message)
       try {
         setBotResponse("Warten ...");      
         const responseData = await response.json();
@@ -86,40 +76,53 @@ function FirstPage() {
       //let [botResponse, setBotResponse] = useState("xx");
   };
 
-  // <TreeControl setterFunc='setInput' />
+  const PP= [
+              "Welche bestehenden und zukünftigen Fördermaßnahmen auf EU- und Bundesebene sind für energieintensive Mittelstandsunternehmen besonders relevant? Analysiere die Wirksamkeit von Investitionszuschüssen, steuerlichen Abschreibungsmodellen und speziellen Industriestromtarifen im Vergleich zu direkten Subventionen. Welche Risiken entstehen durch Fehlanreize (z. B. „Carbon Leakage“ oder Mitnahmeeffekte), und wie könnte eine intelligente Förderpolitik ausgestaltet werden, um sowohl Klimaziele als auch die Wettbewerbsfähigkeit der deutschen Industrie zu sichern?",
+              "Inwiefern können KI-gestützte Energiemanagementsysteme dazu beitragen, den Stromverbrauch in mittelständischen Produktionsbetrieben signifikant zu reduzieren? Untersuche dabei nicht nur die technischen Möglichkeiten (z. B. Demand Response, Lastprognosen, intelligente Produktionssteuerung), sondern auch regulatorische und wirtschaftliche Hindernisse bei der Implementierung. Welche staatlichen Förderprogramme oder steuerlichen Anreize könnten die Verbreitung solcher Technologien beschleunigen, und wie lassen sich Bürokratiehürden für KMU abbauen?",
+              "Wie können mittelständische Industrieunternehmen in Deutschland langfristig wettbewerbsfähig bleiben, wenn gleichzeitig die Energiepreise steigen und subventionierte Stromtarife für Großkonzerne die Marktdynamik verzerren? Welche regulatorischen Anpassungen wären notwendig, um eine gerechtere Verteilung von Energiekosten zu ermöglichen, ohne Investitionen in erneuerbare Energien und Netzstabilität zu gefährden? Analysiere internationale Modelle, insbesondere aus den USA und China, und entwickle eine wirtschaftspolitische Strategie für Deutschland.",
+              "Wie unterscheiden sich die Energiesteuersysteme in Deutschland, der EU und den USA hinsichtlich fossiler Brennstoffe und erneuerbarer Energien? Welche wirtschaftlichen und ökologischen Auswirkungen haben unterschiedliche Steuersätze auf Verbraucher und Industrie?",
+              "Welche Rolle spielen das Erneuerbare-Energien-Gesetz (EEG) und das Energieeffizienzgesetz (EnEfG) bei der Erreichung der Klimaziele? Welche Änderungen wären notwendig, um den Ausbau erneuerbarer Energien noch schneller voranzutreiben?",
+              "Welche konkreten Technologien und Prozessoptimierungen können energieintensive Industriezweige (z. B. Stahl-, Chemie-, oder Halbleiterproduktion) nutzen, um ihren Stromverbrauch signifikant zu reduzieren? Wie wirksam sind bestehende regulatorische Anreize?",
+              "Welche nationalen und EU-weiten Förderprogramme existieren für Unternehmen und Privathaushalte zur Umstellung auf Solar-, Wind- oder Geothermie-Anlagen? Welche wirtschaftlichen Faktoren beeinflussen die Akzeptanz dieser Förderungen?",
+              "Welche regulatorischen Anpassungen sind erforderlich, um Smart Grids effizient in bestehende Stromnetze zu integrieren? Welche steuerlichen oder fördertechnischen Maßnahmen könnten Anreize für Investitionen in intelligente Netzinfrastrukturen schaffen?",
+              "Wie kann eine reformierte Strombesteuerung Haushalte mit niedrigem Einkommen entlasten, ohne den Anreiz zur Energieeinsparung zu verringern? Welche internationalen Best Practices könnten als Vorbild dienen?",
+              "Wie lässt sich die Energiesicherheit eines Landes gewährleisten, während gleichzeitig fossile Subventionen reduziert und erneuerbare Energien gefördert werden? Welche rechtlichen und steuerlichen Instrumente könnten eine stabile Übergangsphase unterstützen?"
+            ]
+
   return (
     <div className="flex flex-col items-center padding-4">
-      <HeaderComp  />
-      <div className="ps-5 pt-2 pe-5 text-center" >
       
+      <div className="ps-5 pt-2 pe-5 text-center" >
+      <HeaderComp />
       <ModelSelector />
-      <h1 className="text-2xl font-bold mb-4">EnerNavi KI Chat Wizard</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col items-center max-w-md">
+      <h2 className="text-2xl font-bold mb-4" style={{color: "#0099cc"}} >EnerNavi KI Chat</h2>
+      <form onSubmit={handleSubmit} className="flex flex-col items-center max-w-md" >
           <textarea
-          className="border border-primary rounded "
-          rows="10" style={{width: "80%"}}
+          className="border border-primary rounded " 
+          rows="12" style={{width: "90%", backgroundColor: "#F9F9F9"}}
+          readOnly
           placeholder="KI Antwort..."
           value={botResponse} 
           onChange={(e) => setBotResponse(e.target.value)}
         />          
-        <br /> <br />
-        <br /> <br />
-
-        <div>
-          <button className="btn m-2 btn-sm btn-outline-success" onClick={(e) => {e.preventDefault(); setInput('Skizziere mir kurz die neueste News im Bereich Energien, Energiepolitik in Deutschland'); setBotResponse(""); return false;}}>Prompt 1</button>
-          <button className="btn m-2 btn-sm btn-outline-success" onClick={(e) => {e.preventDefault(); setInput('Sag ganz kurz was über Solarenergie!'); setBotResponse(""); return false;}}>Prompt 2</button>
-          <button className="btn m-2 btn-sm btn-outline-success" onClick={(e) => {e.preventDefault(); setInput('Sag ganz kurz was über Windenergie!'); setBotResponse(""); return false;}}>Prompt 3</button>
-          <button className="btn m-2 btn-sm btn-outline-success" onClick={(e) => {e.preventDefault(); setInput('Sag ganz kurz was über Biogas!'); setBotResponse(""); return false;}}>Biogas</button>
-          <button className="btn m-2 btn-sm btn-outline-success" onClick={(e) => {e.preventDefault(); setInput('Sag ganz kurz was über Fördergelder!'); setBotResponse(""); return false;}}>Fördergelder</button>
-          <button className="btn m-2 btn-sm btn-outline-success" onClick={(e) => {e.preventDefault(); setInput('Sag ganz kurz was über Gesetzesänderungen für Energieverbraucher!'); setBotResponse(""); return false;}}>Verbraucher</button>
-          <button className="btn m-2 btn-sm btn-outline-success" onClick={(e) => {e.preventDefault(); setInput('Wie kann ich mein Stromverbrauch als Gewerbetreibende  optimieren?'); setBotResponse(""); return false;}}>Optimieren</button>
+        <div style={{paddingTop: "30px"}} >
+          <button className="btn m-2 btn-sm btn-outline-success" onClick={(e) => {e.preventDefault(); setInput(PP[0]); setBotResponse(""); return false;}}>Prompt 1</button>
+          <button className="btn m-2 btn-sm btn-outline-success" onClick={(e) => {e.preventDefault(); setInput(PP[1]); setBotResponse(""); return false;}}>Prompt 2</button>
+          <button className="btn m-2 btn-sm btn-outline-success" onClick={(e) => {e.preventDefault(); setInput(PP[2]); setBotResponse(""); return false;}}>Prompt 3</button>
+          <button className="btn m-2 btn-sm btn-outline-success" onClick={(e) => {e.preventDefault(); setInput(PP[3]); setBotResponse(""); return false;}}>Prompt 4</button>
+          <button className="btn m-2 btn-sm btn-outline-success" onClick={(e) => {e.preventDefault(); setInput(PP[4]); setBotResponse(""); return false;}}>Prompt 5</button>
+          <button className="btn m-2 btn-sm btn-outline-success" onClick={(e) => {e.preventDefault(); setInput(PP[5]); setBotResponse(""); return false;}}>Prompt 6</button>
+          <button className="btn m-2 btn-sm btn-outline-success" onClick={(e) => {e.preventDefault(); setInput(PP[6]); setBotResponse(""); return false;}}>Prompt 7</button>
+          <button className="btn m-2 btn-sm btn-outline-success" onClick={(e) => {e.preventDefault(); setInput(PP[7]); setBotResponse(""); return false;}}>Prompt 8</button>
+          <button className="btn m-2 btn-sm btn-outline-success" onClick={(e) => {e.preventDefault(); setInput(PP[8]); setBotResponse(""); return false;}}>Prompt 9</button>
+          <button className="btn m-2 btn-sm btn-outline-success" onClick={(e) => {e.preventDefault(); setInput(PP[9]); setBotResponse(""); return false;}}>Prompt 10</button>
         </div>        
         <textarea
           className="border border-primary rounded mb-4"
-          rows="4"
+          rows="5"
           placeholder="Frage stellen..."
           value={input}
-          style={{width: "65%"}}
+          style={{width: "80%"}}
           onChange={(e) => setInput(e.target.value)}
         />
         <br />
@@ -127,7 +130,7 @@ function FirstPage() {
           type="submit" className="btn btn-primary bg-xblue-500 text-black-100 py-2 px-4 rounded hover:bg-blue-600"
           disabled={loading}
           onClick={() => {handleSubmit(this); return false;}       } 
-        >{loading ? 'Warten...' : 'Absenden'}</button>
+        >{loading ? 'Warten...' : 'Go'}</button>
           <p /> <br />
 
       </form>
@@ -136,9 +139,6 @@ function FirstPage() {
     </div>
   );
 }
-
-
-// Second Page Component
 
 
 function App() {
@@ -171,14 +171,6 @@ function App() {
   )
 }
 
-/*************  ✨ Codeium Command ⭐  *************/
-  /**
-   * The ModelSelector component.
-   *
-   * This component renders a dropdown menu (select HTML element) that
-   * allows the user to select a GPT model. The model options are hardcoded.
-   * The selected model is stored in the component's state.
-/******  84ec35bb-cc73-4a74-8385-1668e3ecfb00  *******/
 function ModelSelector() {
 
   const [selectedGPT, setSelectedGPT] = useState("1");
@@ -188,8 +180,8 @@ function ModelSelector() {
   };
   return (
     <div className="float-right">
-    <center style={{fontSize: "12pt"}}>
-      <select value={selectedGPT} onChange={handleChange} className="form-select"  selected="2" aria-label="Selector" style={{width: "350px", float: "right" }}>
+    <center style={{fontSize: "9px", color: "#0099cc"}} >
+      <select value={selectedGPT} onChange={handleChange} className="form-select"  selected="2" aria-label="Selector" style={{width: "300px", marginTop: "10px", fontSize: "11px", float: "right" }}>
       <option value="0">GPT Auswählen ... </option>
       <option value="1">gpt-4o</option>
       <option value="2">dall-e-2</option>
--- src/HeaderComp.jsx
+++ src/HeaderComp.jsx
@@ -34,7 +34,7 @@ export default function HeaderComp() {
                 <NavDropdown.Item target='_blank' href="/about">FAQ</NavDropdown.Item>
                 <NavDropdown.Item target='_blank' href="/about">Tutorials</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item target='_blank' href="/about">Versionsinfos</NavDropdown.Item>
+                <NavDropdown.Item target='_blank' href="/about">Version</NavDropdown.Item>
               </NavDropdown>
 
             </Nav>
